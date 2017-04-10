aa34010a3d5f96e5584141d8921419fbc4293b94
GWD <347757054@qq.com>
2017-04-10 14:32:25 +0800

    部分调整

---
+++ BuDeJie/BuDeJie/Classes/Other/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BuDeJie
+//
+//  Created by 古伟东 on 2017/4/10.
+//  Copyright © 2017年 victorgu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
