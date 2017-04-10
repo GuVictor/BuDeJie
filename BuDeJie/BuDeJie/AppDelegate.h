d5cc6fe403ba2c017a52695bf47f55a2c6080bb5
GWD <347757054@qq.com>
2017-04-10 09:07:56 +0800

    初始化项目，导入素材

---
+++ BuDeJie/BuDeJie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BuDeJie
+//
+//  Created by 古伟东 on 2017/4/10.
+//  Copyright © 2017年 victorgu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BuDeJie/BuDeJie/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  BuDeJie
+//
+//  Created by 古伟东 on 2017/4/10.
+//  Copyright © 2017年 victorgu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+
+@end
