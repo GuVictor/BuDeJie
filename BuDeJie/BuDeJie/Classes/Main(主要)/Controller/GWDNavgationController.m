977040d8301cd614412ceebe6fbd68d1222c02d1
GWD <347757054@qq.com>
2017-04-13 15:09:54 +0800

    实现边缘滑动返回 self.interactivePopGestureRecognizer.delegate = self; 还要解决在跟空气 滑动的bug

--- BuDeJie/BuDeJie/Classes/Main(主要)/Controller/GWDNavgationController.m
+++ BuDeJie/BuDeJie/Classes/Main(主要)/Controller/GWDNavgationController.m
@@ -8,7 +8,7 @@
 
 #import "GWDNavgationController.h"
 
-@interface GWDNavgationController ()
+@interface GWDNavgationController ()<UIGestureRecognizerDelegate>
 
 @end
 
@@ -34,11 +34,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.interactivePopGestureRecognizer.delegate = self;
+    
     
 }
 
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
     if (self.childViewControllers.count > 0) { //非根控制器
+        
+        //恢复滑动返回功能 -> 分析：把系统的返回按钮覆盖 -> 1.手势失效(1.手势被清空 2.可能手势代理做了一些事情，导致手势失效)
         
         //设置返回按钮，只有非根控制器
         viewController.navigationItem.leftBarButtonItem = [UIBarButtonItem backItemWithImage:[UIImage imageNamed:@"navigationButtonReturn"] highImage:[UIImage imageNamed:@"navigationButtonReturnClick"] target:self action:@selector(back) title:@"返回"];
@@ -49,5 +53,9 @@
 
 - (void)back {
     [self popViewControllerAnimated:YES];
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch {
+    return self.childViewControllers.count > 1;
 }
 @end
